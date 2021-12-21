b66e28d4a927ade9246cdb63c2147de043062050
xiaotiandada <istianlei@qq.com>
2021-12-22 01:54:12 +0800

    feat: :+1: contract

---
+++ learn/contracts/Box.sol
@@ -0,0 +1,24 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+  uint256 private _value;
+
+  // Emitted when the stored value changes
+  event ValueChanged(uint256 value);
+
+  // Stores a new value in the contract
+  function store(uint256 value) public onlyOwner {
+    _value = value;
+    emit ValueChanged(value);
+  }
+
+  // Reads the last stored value
+  function retrieve() public view returns (uint256) {
+    return _value;
+  }
+}
